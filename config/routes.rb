d177a2bb8f45a11375fdc247812b1fd053e682b5
Kevin Korte <kevinkorte15@gmail.com>
2015-11-25 00:57:38 +0000

    defined roots for logged in users

--- config/routes.rb
+++ config/routes.rb
@@ -2,17 +2,20 @@ Rails.application.routes.draw do
   #devise_for :users
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
-
-  # You can have the root of your site routed with "root"
+  devise_for :users
+  # resources :users, only: [:show]
+  #   as :user do
+  #     get 'signin' => 'devise/sessions#new', :as => :new_user_session
+  #     post 'signin' => 'devise/sessions#create', :as => :user_session
+  #     delete 'signout' => 'devise/sessions#destroy', :as => :destroy_user_session
+  #   end
+  
+  authenticated :user do
+    root 'user#show', as: :authenticated_root
+  end
     root 'static#home'
-
-  devise_for :users, :skip => [:sessions]
-  resources :users, only: [:show]
-    as :user do
-      get 'signin' => 'devise/sessions#new', :as => :new_user_session
-      post 'signin' => 'devise/sessions#create', :as => :user_session
-      delete 'signout' => 'devise/sessions#destroy', :as => :destroy_user_session
-    end
+  
+  
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
